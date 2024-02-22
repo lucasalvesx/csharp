0a10e8abdf8d6ec356622acd57cbbfc274fbbf2e
lucasalvesx <lucassalves005@gmail.com>
2024-02-22 16:08:50 -0300

    220224

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 ﻿//Primeiro, declaram-se as variáveis
-string N, S;
+//String é o tipo da variável
+string Nome, Sobrenome;
 
 //Pedimos ao usuário para inserir o valor (nome)
 Console.WriteLine("Ur first Name");
 //Declaramos a variável a qual vai ser atribuído o valor
-N = Console.ReadLine();
+Nome = Console.ReadLine();
 
 //Mesma coisa, agora para segunda variável (sobrenome)
 Console.WriteLine("Now your last name");
 //Atribuindo o valor inserido
-S = Console.ReadLine();
+Sobrenome = Console.ReadLine();
 
 //Imprimindo o resultado do conteudo inserido
-Console.WriteLine("{0} {1}", N, S);
+Console.Write("{0} {1}", Nome, Sobrenome);
 // O 0 e 1 entre chaves são necessários para indicar a ordem da exibição das duas variáveis, sendo 0 o primeiro
